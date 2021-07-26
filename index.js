7900d85a1d5f82f6a7013c4a26ede592cfc57bf8
GiuseppePennisi <jojop101@gmail.com>
2021-07-26 15:40:47 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function introduction(name) {
+    
+    return(`Hi, my name is ${name}.`);
+}
+introduction("Josh");
+
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+introductionWithLanguage("Josh", "Ember.Josh")
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+introductionWithLanguageOptional("Josh")
